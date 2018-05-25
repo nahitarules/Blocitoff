e889e42641b39573ee69cf691ca2c24486359a5e
nahome <nahom.teshome@yahoo.com>
2018-05-24 17:39:28 -0700

    user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root 'users#show'
+
   devise_for :users
   get 'welcome/about'
 
-  root 'welcome#index'
+  get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
